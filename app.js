37849e7e6fed34319b91e920d4fde7011fc5ef72
edoardobinda <edoardobinda85@gmail.com>
2022-03-10 11:30:48 +0100

    update keyboard toUpperCase

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //VAR DECLARATION
 
-var array = ["STELO"];
+var array = ["TREMO"];
 var arrayAllCapital = [];
 var selected = "";
 var arrayCapShufNotRepeated = [];
@@ -24,8 +24,8 @@ window.onload = function (){
     selected = arrayAllCapital[0];
     console.log(selected); // This is the SELECTED word that will be on the display
     arrayForSelected = selected.split("");
-    
-    
+
+
 
 } //ONLOAD FUNCTION ENDS HERE /////////////////////////////////////////////////////////////////
 
@@ -37,8 +37,11 @@ var div5 = document.getElementById("divdisplay5");
 let chosenword = "";
 
 
+
 for (let i = 1; i < 27; i++) {
+
     let buttoninput = document.getElementById("divinput"+i);
+
     buttoninput.onclick = function (){
         if (div1.innerText == ""){
             div1.innerText = this.innerText;
@@ -59,23 +62,47 @@ for (let i = 1; i < 27; i++) {
     }
 }// END OF FOR LOOP
 
+
+const log = document.getElementById("log");
+
+document.addEventListener("keydown", logKey);
+
+
+    function logKey(e) {
+            if (div1.innerText == ""){
+                div1.innerText = e.key.toUpperCase();
+            }
+            else if (div2.innerText == ""){
+                div2.innerText = e.key.toUpperCase();
+            }
+            else if (div3.innerText == ""){
+                div3.innerText = e.key.toUpperCase();
+            }
+            else if (div4.innerText == ""){
+                div4.innerText = e.key.toUpperCase();
+            }
+            else if (div5.innerText == ""){
+                div5.innerText = e.key.toUpperCase();
+            }
+            chosenword = div1.innerText + div2.innerText +div3.innerText +div4.innerText +div5.innerText;
+        }
+
+
+
 //SUBMIT BUTTON
 function submit(){
+
     arrayForChosen = chosenword.split("");
     let control = [];
 
     for (let i = 0; i < arrayForChosen.length; i++){
 
-        var arrayletterTofindinChosen = [];
         var letterToFindInSelected = [...selected].filter(l => l === arrayForChosen[i]).length;
         var letterTofindinChosen = [...chosenword].filter(l => l === arrayForChosen[i]).length;
 
-
-        if (letterTofindinChosen > 1){
-            arrayletterTofindinChosen.push(letterTofindinChosen);
+        /*if (letterTofindinChosen > 1){
             control.push(arrayForChosen[i]);
-        }
-        
+        }*/
 
         // SE LE LETTERE ALLA POSIZIONE "i" SONO IDENTICHE ==== V E R D E !!!
         if (arrayForChosen[i] == arrayForSelected[i]){
@@ -91,9 +118,10 @@ function submit(){
 
         //POSSIBILITA' DI LETTERE GIALLE
         else {
-            if ((letterTofindinChosen > letterToFindInSelected) && (control.length == 1)){
+            if (((letterTofindinChosen > letterToFindInSelected) && (0 <= control.length) && (control.length <= 1))){
                 yellowarray.push(arrayForChosen[i]);
-                document.getElementById("divdisplay"+(i+1)).setAttribute("class", "maybeLetter");        
+                document.getElementById("divdisplay"+(i+1)).setAttribute("class", "maybeLetter");
+                control.push(arrayForChosen[i]);
             }
             else if ((letterTofindinChosen > letterToFindInSelected) && (control.length > 1)){
                 redarray.push(arrayForChosen[i]);
@@ -102,11 +130,13 @@ function submit(){
             else if ((letterTofindinChosen == letterToFindInSelected) && (arrayForChosen[i] != arrayForSelected[i])){
                 yellowarray.push(arrayForChosen[i]);
                 document.getElementById("divdisplay"+(i+1)).setAttribute("class", "maybeLetter");
+                control.push(arrayForChosen[i]);
             }
-            else {
+            /*else {
                 yellowarray.push(arrayForChosen[i]);
                 document.getElementById("divdisplay"+(i+1)).setAttribute("class", "maybeLetter");
-            }
+                control.push(arrayForChosen[i]);
+            }*/
         }
     }/*LOOP ENDS*/
 } /*FUNCTION ENDS*/
